ccc5d5faf5c3fc5b8ec6e1ae02dc33bb6b756642
Huynh Tan Loc <huynhtanlocpp09@gmail.com>
2026-07-16 16:21:42 +0700

    Fix session refresh after PayOS redirect

--- Front_end/my-project/src/Components/Navbar.jsx
+++ Front_end/my-project/src/Components/Navbar.jsx
@@ -212,7 +212,7 @@ const Navbar = ({ userInfo }) => {
 
   return (
     <>
-      <div className={`fixed top-4 left-0 right-0 z-[100] flex px-6 lg:px-12 pointer-events-none transition-all duration-500 ${
+      <div className={`fixed top-4 left-0 right-0 z-[1000] flex px-6 lg:px-12 pointer-events-none transition-all duration-500 ${
         isScrolled ? "justify-center lg:justify-start" : "justify-center"
       }`}>
         <motion.nav
